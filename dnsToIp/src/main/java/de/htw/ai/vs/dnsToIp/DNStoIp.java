9dc7b2b04207752d860b0516274e2889913108ac
Alioio <progmaster11gmail.com>
2018-11-18 16:20:10 +0100

    adding method IpToDNS

--- dnsToIp/src/main/java/de/htw/ai/vs/dnsToIp/DNStoIp.java
+++ dnsToIp/src/main/java/de/htw/ai/vs/dnsToIp/DNStoIp.java
@@ -6,9 +6,19 @@ import java.net.UnknownHostException;
 public class DNStoIp {
 
 	public static void main(String[] args) {
-		
+
+		// args empty?
+		// to short?
+
+		getDNSofIP(args[0]);
+		getDNSofIP(args[0]);
+
+	}
+
+	protected static void getIPofDNS(String hostname) {
+
 		try {
-			InetAddress inetAddr = InetAddress.getByName(args[0]);
+			InetAddress inetAddr = InetAddress.getByName(hostname);
 			byte[] addr = inetAddr.getAddress();
 			// Convert to dot representation
 			String ipAddr = "";
@@ -19,9 +29,25 @@ public class DNStoIp {
 				ipAddr += addr[i] & 0xFF;
 			}
 			System.out.println("IP Address: " + ipAddr);
-		}catch (UnknownHostException e) {
+		} catch (UnknownHostException e) {
 			System.out.println("Host not found: " + e.getMessage());
-		}	
+		}
+
+	}
+
+	protected static void getDNSofIP(String ipAdress) {
+
+		try {
+			InetAddress inetAddr = InetAddress.getByName(ipAdress);
+			// Get the host name
+			String hostname = inetAddr.getHostName();
+			// Get canonical host name
+			String canonicalHostname = inetAddr.getCanonicalHostName();
+			System.out.println("Hostname: " + hostname);
+			System.out.println("Canonical Hostname: " + canonicalHostname);
+		} catch (UnknownHostException e) {
+			System.out.println("Host not found: " + e.getMessage());
+		}
 	}
 
 }
