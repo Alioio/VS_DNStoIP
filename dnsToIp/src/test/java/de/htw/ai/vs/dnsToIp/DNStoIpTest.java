78d06d1e8a7500306344f61e5b9b7457cd506812
Alioio <progmaster11gmail.com>
2018-11-21 14:59:51 +0100

    adding test cases

--- dnsToIp/src/test/java/de/htw/ai/vs/dnsToIp/DNStoIpTest.java
+++ dnsToIp/src/test/java/de/htw/ai/vs/dnsToIp/DNStoIpTest.java
@@ -7,11 +7,37 @@ import org.junit.jupiter.api.Test;
 public class DNStoIpTest {
 	
 	DNStoIp dns;
-	
-
 
 	@Test
-	public void test1(){
+	public void correntIPprovided(){
+		
+		
+		Assertions.assertEquals(dns.getDNSofIP("ip von htw berlin"), dns.getIPofDNS(hostname));
+	}	
+	
+	@Test
+	public void correntDNSprovided(){
 		Assertions.assertEquals(2, 1+1);
 	}
+	
+	@Test
+	public void correntNotExistingIPprovided(){
+		Assertions.assertEquals(2, 1+1);
+	}
+	
+	@Test
+	public void correntNotExistingDNSprovided(){
+		Assertions.assertEquals(2, 1+1);
+	}
+	
+	@Test
+	public void noArgumentProvided(){
+		Assertions.assertEquals(2, 1+1);
+	}
+	
+	@Test
+	public void wrongArgument(){
+		Assertions.assertEquals(2, 1+1);
+	}
+	
 }
